2713f7582312a120b315b791a3564dd4904d614e
Murdho Savila <murdho@murdho.com>
2024-04-21 19:51:16 +0300

    Make Turbo::Debouncer execute task after debouncing for a while
    Not ideal, as there's no synchronization between threads. Thus, for a
    while all is nice and quiet, and suddenly every debouncer allows the
    task to finish, causing "floods".
    
    While thinking about it, I'm thinking of trying an alternative to
    debouncer: throttler. Ideally, the throttler would use global executor,
    which would be able to "rate limit" the specific task across threads.
    
    What I'm after is that in case of many broadcasts (that are debounced),
    after a little while, one would get to execute and deliver updates to
    consumers. This way UI could still update withing reason, giving user a
    nicer experience.

--- app/models/turbo/debouncer.rb
+++ app/models/turbo/debouncer.rb
@@ -1,24 +1,39 @@
 class Turbo::Debouncer
-  attr_reader :delay, :scheduled_task
+  attr_reader :delay, :max_delay, :delaying_since, :scheduled_task
 
   DEFAULT_DELAY = 0.5
+  DEFAULT_MAX_DELAY = 5
 
-  def initialize(delay: DEFAULT_DELAY)
+  def initialize(delay: DEFAULT_DELAY, max_delay: DEFAULT_MAX_DELAY)
     @delay = delay
+    @max_delay = max_delay
+    @delaying_since = nil
     @scheduled_task = nil
   end
 
   def debounce(&block)
+    wait if delayed_long_enough?
     scheduled_task&.cancel unless scheduled_task&.complete?
     @scheduled_task = Concurrent::ScheduledTask.execute(delay, &block)
+    @delaying_since ||= now if max_delay
   end
 
   def wait
-    scheduled_task&.wait(wait_timeout)
+    scheduled_task&.wait(wait_timeout).tap do
+      @delaying_since = nil
+    end
   end
 
   private
     def wait_timeout
       delay + 1
+    end
+
+    def delayed_long_enough?
+      delaying_since && now - delaying_since >= max_delay
+    end
+
+    def now
+      Concurrent.monotonic_time :second
     end
 end
